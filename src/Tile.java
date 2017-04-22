cfd33ca8d25d5791de83c68a21e4e6d59381c0aa
Ryguy9999 <ryancirincione@aol.com>
2017-04-22 18:29:46 -0400

    Texture code

--- src/Tile.java
+++ src/Tile.java
@@ -1,5 +1,16 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 public enum Tile 
 {
-	Grass, Dirt, Water, Rocks, Cement, Sand
+	Grass("res/grass.png"), Dirt("res/dirt.png"), Water("res/water.png"), Rocks("res/rock.png"),
+	Cement("res/cement.png"), Sand("res/sand.png");
+	
+	public BufferedImage img;
+	Tile(String s)
+	{
+		img = ImageIO.read(new File(s));
+	}
 }
