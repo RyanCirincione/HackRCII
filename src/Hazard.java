e6b05d7312b03590813416f23f2199e3718ca2c4
Ryan Goldstein <ryanisaacg@gmail.com>
2017-04-23 04:05:37 -0400

    Switched from Graphics2D to Graphics

--- src/Hazard.java
+++ src/Hazard.java
@@ -1,9 +1,9 @@
-import java.awt.Graphics2D;
+import java.awt.Graphics;
 import java.awt.Point;
 
 public interface Hazard 
 {
 	boolean hitsLlama(Point llama);
 	void step();
-	void draw(Graphics2D g);
+	void draw(Graphics g);
 }
