a6dc24079e2af61aaacb75af27c6b30da62d436a
Ryan Goldstein <ryanisaacg@gmail.com>
2017-04-23 03:42:13 -0400

    Refactored into using a getTileX and getTileY function

--- src/HackRCIIMain.java
+++ src/HackRCIIMain.java
@@ -67,38 +67,19 @@ public class HackRCIIMain extends JPanel
 		}
 		
 		tiles = new ArrayList<Tile>();
-		tiles.add(new Tile('q', KEYBOARD_START_X + BUFFER*1 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 1/2, KEYBOARD_START_Y + BUFFER*1 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 1/2));
-		tiles.add(new Tile('w', KEYBOARD_START_X + BUFFER*2 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 3/2, KEYBOARD_START_Y + BUFFER*1 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 1/2));
-		tiles.add(new Tile('e', KEYBOARD_START_X + BUFFER*3 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 5/2, KEYBOARD_START_Y + BUFFER*1 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 1/2));
-		tiles.add(new Tile('r', KEYBOARD_START_X + BUFFER*4 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 7/2, KEYBOARD_START_Y + BUFFER*1 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 1/2));
-		tiles.add(new Tile('t', KEYBOARD_START_X + BUFFER*5 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 9/2, KEYBOARD_START_Y + BUFFER*1 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 1/2));
-		tiles.add(new Tile('y', KEYBOARD_START_X + BUFFER*6 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 11/2, KEYBOARD_START_Y + BUFFER*1 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 1/2));
-		tiles.add(new Tile('u', KEYBOARD_START_X + BUFFER*7 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 13/2, KEYBOARD_START_Y + BUFFER*1 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 1/2));
-		tiles.add(new Tile('i', KEYBOARD_START_X + BUFFER*8 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 15/2, KEYBOARD_START_Y + BUFFER*1 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 1/2));
-		tiles.add(new Tile('o', KEYBOARD_START_X + BUFFER*9 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 17/2, KEYBOARD_START_Y + BUFFER*1 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 1/2));
-		tiles.add(new Tile('p', KEYBOARD_START_X + BUFFER*10 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 19/2, KEYBOARD_START_Y + BUFFER*1 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 1/2));
+		char[][] letters = {
+				{'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'},
+				{'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'},
+				{'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'}
+		};
+		for(int y = 0; y < letters.length; y++)
+		{
+			for(int x = 0; x < letters[y].length; x++)
+			{
+				tiles.add(new Tile(letters[y][x], getAbsX(x), getAbsY(y)));
+			}
+		}
 
-		tiles.add(new Tile('a', KEYBOARD_START_X + BUFFER*1 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 1/2, KEYBOARD_START_Y + BUFFER*2 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 3/2));
-		tiles.add(new Tile('s', KEYBOARD_START_X + BUFFER*2 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 3/2, KEYBOARD_START_Y + BUFFER*2 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 3/2));
-		tiles.add(new Tile('d', KEYBOARD_START_X + BUFFER*3 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 5/2, KEYBOARD_START_Y + BUFFER*2 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 3/2));
-		tiles.add(new Tile('f', KEYBOARD_START_X + BUFFER*4 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 7/2, KEYBOARD_START_Y + BUFFER*2 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 3/2));
-		tiles.add(new Tile('g', KEYBOARD_START_X + BUFFER*5 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 9/2, KEYBOARD_START_Y + BUFFER*2 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 3/2));
-		tiles.add(new Tile('h', KEYBOARD_START_X + BUFFER*6 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 11/2, KEYBOARD_START_Y + BUFFER*2 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 3/2));
-		tiles.add(new Tile('j', KEYBOARD_START_X + BUFFER*7 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 13/2, KEYBOARD_START_Y + BUFFER*2 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 3/2));
-		tiles.add(new Tile('k', KEYBOARD_START_X + BUFFER*8 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 15/2, KEYBOARD_START_Y + BUFFER*2 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 3/2));
-		tiles.add(new Tile('l', KEYBOARD_START_X + BUFFER*9 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 17/2, KEYBOARD_START_Y + BUFFER*2 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 3/2));
-		tiles.add(new Tile(';', KEYBOARD_START_X + BUFFER*10 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 19/2, KEYBOARD_START_Y + BUFFER*2 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 3/2));
-
-		tiles.add(new Tile('z', KEYBOARD_START_X + BUFFER*1 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 1/2, KEYBOARD_START_Y + BUFFER*3 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 5/2));
-		tiles.add(new Tile('x', KEYBOARD_START_X + BUFFER*2 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 3/2, KEYBOARD_START_Y + BUFFER*3 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 5/2));
-		tiles.add(new Tile('c', KEYBOARD_START_X + BUFFER*3 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 5/2, KEYBOARD_START_Y + BUFFER*3 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 5/2));
-		tiles.add(new Tile('v', KEYBOARD_START_X + BUFFER*4 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 7/2, KEYBOARD_START_Y + BUFFER*3 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 5/2));
-		tiles.add(new Tile('b', KEYBOARD_START_X + BUFFER*5 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 9/2, KEYBOARD_START_Y + BUFFER*3 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 5/2));
-		tiles.add(new Tile('n', KEYBOARD_START_X + BUFFER*6 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 11/2, KEYBOARD_START_Y + BUFFER*3 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 5/2));
-		tiles.add(new Tile('m', KEYBOARD_START_X + BUFFER*7 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 13/2, KEYBOARD_START_Y + BUFFER*3 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 5/2));
-		tiles.add(new Tile(',', KEYBOARD_START_X + BUFFER*8 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 15/2, KEYBOARD_START_Y + BUFFER*3 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 5/2));
-		tiles.add(new Tile('.', KEYBOARD_START_X + BUFFER*9 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 17/2, KEYBOARD_START_Y + BUFFER*3 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 5/2));
-		tiles.add(new Tile('/', KEYBOARD_START_X + BUFFER*10 + (KEYBOARD_WIDTH - 11*BUFFER)/10 * 19/2, KEYBOARD_START_Y + BUFFER*3 + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * 5/2));
 		this.addKeyListener(new KeyAdapter(){
 			public void keyPressed(KeyEvent e)
 			{
@@ -117,6 +98,16 @@ public class HackRCIIMain extends JPanel
 		});
 		this.setFocusable(true);
 		this.requestFocus();
+	}
+	
+	public int getAbsX(int tileX)
+	{
+		return KEYBOARD_START_X + BUFFER*(tileX+1) + (KEYBOARD_WIDTH - 11*BUFFER)/10 * ((tileX * 2) + 1)/2;
+	}
+	
+	public int getAbsY(int tileY)
+	{
+		return KEYBOARD_START_Y + BUFFER*(tileY + 1) + (KEYBOARD_HEIGHT - 4*BUFFER)/3 * ((tileY * 2) + 1)/2; 
 	}
 	
 	public void tick()
